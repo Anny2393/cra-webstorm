c4592145931d64abc99e87e16fe2b76c2f12927a
Anna Narushevich <annyit23@gmail.com>
2023-01-08 21:26:01 +0300

    14. Build Objects

---
+++ src/FREECODECAMP/14. Build JavaScript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Sam",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Anna", "Cat"]
+
+    // Only change code above this line
+};
