ae209cde131bd61f698c83218f830b1a1cf9b1a1
Anna Narushevich <annyit23@gmail.com>
2023-01-09 22:19:21 +0300

    15. Manipulating complex obj

---
+++ src/FREECODECAMP/15. Manipulating Complex Objects.js
@@ -0,0 +1,22 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "AC/DC",
+        "title": "Highway to Hell",
+        "release_year": 1979,
+        "formats": [
+            "CD",
+            "LP"
+        ]
+    }
+];
